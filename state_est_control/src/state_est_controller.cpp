4666830e585e29a6d9c78cf2a4813c34f6f67c76
Yisha <yisha@yisha-X555LAB>
2016-05-07 01:12:36 +0800

    update

--- state_est_control/src/state_est_controller.cpp
+++ state_est_control/src/state_est_controller.cpp
@@ -2,14 +2,18 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <XmlRpcException.h>
 
+const int FLOOR_LENGTH = 5;
+
 namespace state_est_control {
 	StateEstControl::StateEstControl(ros::NodeHandle& nh, ros::NodeHandle& pnh) : pos_diff(Point(0, 0)), pos(Point(0, 0)) {
 
 		// init tag array
-		tag_array.push_back(Point(0, 0));
-		tag_array.push_back(Point(1, 0));
-		tag_array.push_back(Point(2, 0));
-		tag_array.push_back(Point(3, 0));
+		const int HALF_LENGTH = FLOOR_LENGTH / 2;
+		for(int y=0; y < FLOOR_LENGTH; y++) {
+			for(int x=0; x < FLOOR_LENGTH; x++)
+				tag_array.push_back(Point(x - HALF_LENGTH, y - HALF_LENGTH));
+		} 
+			
 
 		// subscriptions
 		pos_sub_ = nh.subscribe("tag_detections_pose", 1, &StateEstControl::PosCallback, this);
@@ -25,43 +29,33 @@ namespace state_est_control {
 	};
 
 	void StateEstControl::PosCallback(const geometry_msgs::PoseArray::ConstPtr& input_pos_array) {
-		printf("In PosCallback\n");
 		geometry_msgs::PoseArray pos_array = *input_pos_array;
 
 		geometry_msgs::Pose last_pose;
-		for(int i=0; i < (sizeof(pos_array)/sizeof(pos_array[0])); i++) {
-			if(pos_array[i])
-				&last_pose = pos_array[i];
+		for(int i=0; i < pos_array.poses.size(); i++) {
+			last_pose = pos_array.poses[i];
 		}
 
-		if(!last_pose) {
-			printf("Empty Pose\n");
-			return;
-		} else {
-			printf("Pose not Empty\n");
-			pos_diff.x = last_pose.position.x;
-			pos_diff.y = last_pose.position.y;
-		}
+		
+		pos_diff.x = last_pose.position.x;
+		pos_diff.y = last_pose.position.y;
+		
 	}
 
 	void StateEstControl::IdCallback(const std_msgs::Int32MultiArray::ConstPtr& input_id_array) {
 		std_msgs::Int32MultiArray id_array = *input_id_array;
 
-		int32_t last_id;
-		for(int i=0; i < (sizeof(id_array)/sizeof(id_array[0])); i++) {
-			if(id_array[i])
-				last_id = id_array[i];
+		int32_t last_id = -1;
+		for(int i=0; i < id_array.data.size(); i++) {
+			if(id_array.data[i] != NULL)
+				last_id = id_array.data[i];
 		}
 
-		if(!last_id) {
-			printf("Empty Id\n");
-			return;
-		} else {
-			printf("Id not Empty\n");
+		if(last_id != -1) {
 			id = last_id;
 
-			pos.x = tag_array[id].x + pos_diff.x;
-			pos.y = tag_array[id].y + pos_diff.y;
+			pos.x = tag_array[id].x - pos_diff.x;
+			pos.y = tag_array[id].y - pos_diff.y;
 
 			geometry_msgs::PoseStamped state_est_pose;
 			state_est_pose.pose.position.x = pos.x;
